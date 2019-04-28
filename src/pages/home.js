ee888256dfbdf5740ad0f29bd2d17ee157cc6045
everton paixao <everton.paixao16@gmail.com>
2019-04-28 20:34:37 -0300

    component home

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,8 +1,15 @@
 import React, { Component } from "react";
 
+import { Header } from "../components/header/index";
+
 class Home extends Component {
   render() {
-    return <div>home</div>;
+    return (
+      <div id="wrapper">
+        <aside className="menu">everton</aside>
+        <Header />
+      </div>
+    );
   }
 }
 
