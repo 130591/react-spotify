ace84d26a45c452b429e9498eb2596c315997826
everton paixao <everton.paixao16@gmail.com>
2019-05-04 20:18:40 -0300

    estrutura de tela de login

---
+++ src/pages/login.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+export default class SingIn extends Component {
+  render() {
+    return (
+      <div className="singIn">
+        <form className="form">
+          <div>
+            <label />
+            <input type="text" className="" />
+          </div>
+          <div>
+            <label />
+            <input type="" className="" />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
