d6ed0cb6f761758e7526b42fba6a6e021afc4c06
everton paixao <everton.paixao16@gmail.com>
2019-05-04 20:19:31 -0300

    modificações no component header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,23 +1,33 @@
 import React from "react";
 
-export const Header = () => (
-  <header className="header">
-    <div className="head">
-      <h1>Spotify</h1>
-    </div>
-    <div className="nav">
-      <div className="container">
-        <input type="search" className="search" />
-        <ul>
-          <li>
-            <img src="" alt="notificação" />
-          </li>
-          <li>
-            <img src="" alt="user" />
-            <span>everton</span>
-          </li>
-        </ul>
+import notification from "../../imagens/notification.svg";
+import user from "../../imagens/man-user.svg";
+
+export const Header = ({ children }) => (
+  <>
+    <header className="header">
+      <div className="head">
+        <h1>Spotify</h1>
       </div>
-    </div>
-  </header>
+      <div className="nav">
+        <div className="container">
+          <input type="search" className="search" />
+          <ul className="nav-icons">
+            <li>
+              <img
+                src={notification}
+                className="nav-icons__notify"
+                alt="notificação"
+              />
+            </li>
+            <li>
+              <img src={user} className="nav-icons__user" alt="user" />
+              <span>everton</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </header>
+    {children}
+  </>
 );
