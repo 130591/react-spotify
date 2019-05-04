892c16d3513ef50c470f8b72da1a0299a01bcc82
everton paixao <everton.paixao16@gmail.com>
2019-05-04 20:20:46 -0300

    criação do components content , navigation e nav-tabs da tela home

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,13 +1,31 @@
 import React, { Component } from "react";
 
 import { Header } from "../components/header/index";
+import { Menu } from "../components/menu/index";
 
 class Home extends Component {
   render() {
     return (
       <div id="wrapper">
-        <aside className="menu">everton</aside>
-        <Header />
+        <Menu />
+        <div className="content">
+          <Header />
+          <section className="navigation">
+            <div className="container">
+              <h2>Navegar</h2>
+              <nav className="nav-tabs">
+                <ul>
+                  <li>Visão Geral</li>
+                  <li>Paradas</li>
+                  <li>Gêneros e Momentos</li>
+                  <li>Lançamentos</li>
+                  <li>Descobrir</li>
+                </ul>
+              </nav>
+            </div>
+          </section>
+          
+        </div>
       </div>
     );
   }
