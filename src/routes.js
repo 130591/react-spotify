3f45cea7d54f3b55f3783bcb816fc193c31383cf
everton paixao <everton.paixao16@gmail.com>
2019-05-04 20:16:55 -0300

    novas rotas

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,16 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router";
+import { BrowserRouter } from "react-router-dom";
 
+// COMPONENTS PAGES
 import Home from "./pages/home";
+import SingIn from "./pages/login";
 
+// ROUTES
 export const Routes = () => (
   <BrowserRouter>
     <Switch>
-      <Route exact path="/" component={<h1>Home</h1>} />
+      <Route exact path="/" component={SingIn} />
       <Route exact path="/home" component={Home} />
     </Switch>
   </BrowserRouter>
