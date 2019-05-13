1432a19da06c21ba5b9204d69fc59d0fe53615a6
everton paixao <everton.paixao16@gmail.com>
2019-05-13 14:41:44 -0300

    page home

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,7 +4,6 @@ import React, { Component } from "react";
 import { Card } from "../components/card/index";
 import { Navigation } from "../components/header/index";
 import { Collections, CollectionGrid } from "../components/colections/index";
-import AudioPlayer from 'react-modular-audio-player';
 
 // ICONS
 import capa from "../imagens/Richie-Kotzen-Mother-Heads-Family-Reunion-1994.jpg";
@@ -12,52 +11,42 @@ import capa from "../imagens/Richie-Kotzen-Mother-Heads-Family-Reunion-1994.jpg"
 class Home extends Component {
   render() {
     return (
-        <div className="content">
-          <Navigation />
-          <Collections title="Lançamentos">
-            <CollectionGrid>
-              <Card Image={capa} />
-              <Card Image={capa} />
-              <Card Image={capa} />
-              <Card Image={capa} />
-              <Card Image={capa} />
-              <Card Image={capa} />
-              <Card Image={capa} />
-              <Card Image={capa} />
-              <Card Image={capa} />
-              <Card Image={capa} />
-            </CollectionGrid>
-          </Collections>
-          <Collections title="Lançamentos">
-            <CollectionGrid>
-              <Card Image={capa} />
-              <Card Image={capa} />
-              <Card Image={capa} />
-              <Card Image={capa} />
-              <Card Image={capa} />
-              <Card Image={capa} />
-              <Card Image={capa} />
-              <Card Image={capa} />
-              <Card Image={capa} />
-              <Card Image={capa} />
-            </CollectionGrid>
-          </Collections>
-          <Collections title="Lançamentos">
-            <CollectionGrid>
-              <Card Image={capa} />
-              <Card Image={capa} />
-              <Card Image={capa} />
-              <Card Image={capa} />
-              <Card Image={capa} />
-              <Card Image={capa} />
-              <Card Image={capa} />
-              <Card Image={capa} />
-              <Card Image={capa} />
-              <Card Image={capa} />
-            </CollectionGrid>
-          </Collections>
-          <AudioPlayer />
-        </div>
+      <div className="content">
+        <Navigation content={['Em Destaque', 'Podcasts', 'paradas', 'Gêneros', 'lançamentos','Descobrir']} />
+        <Collections title="Lançamentos">
+          <CollectionGrid>
+            <Card Image={capa} />
+            <Card Image={capa} />
+            <Card Image={capa} />
+            <Card Image={capa} />
+            <Card Image={capa} />
+            <Card Image={capa} />
+            <Card Image={capa} />
+            <Card Image={capa} />
+            <Card Image={capa} />
+            <Card Image={capa} />
+          </CollectionGrid>
+        </Collections>
+        <Collections title="Lançamentos">
+          <CollectionGrid>
+            <Card Image={capa} />
+            <Card Image={capa} />
+            <Card Image={capa} />
+            <Card Image={capa} />
+            <Card Image={capa} />
+            <Card Image={capa} />
+          </CollectionGrid>
+        </Collections>
+        <Collections title="Lançamentos">
+          <CollectionGrid>
+            <Card Image={capa} />
+            <Card Image={capa} />
+            <Card Image={capa} />
+            <Card Image={capa} />
+            <Card Image={capa} />
+          </CollectionGrid>
+        </Collections>
+      </div>
     );
   }
 }
