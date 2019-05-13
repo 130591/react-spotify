231550d8bcebac2fb4c6a14b335fb1d4f0b359a2
everton paixao <everton.paixao16@gmail.com>
2019-05-13 14:35:34 -0300

    component player

--- src/components/player/index.js
+++ src/components/player/index.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React from "react";
 
-import { Slider, Direction } from 'react-player-controls';
+import play from '../../imagens/play-button.svg';
+import pause from '../../imagens/pause.svg';
+import left from '../../imagens/fast-forward-button (1).svg';
 
 export const Player = () => {
   return (
-  <section className="player">
-    <Slider />
-    <Direction />
-  </section>
+    <section className="player">
+      <ul className="controls">
+        <li><img src={left} className="controls__left" alt="left"/></li>
+        <li>
+          <img src={play} className="controls__play" alt="play"/>
+        </li>
+        <li><img src={left} className="controls__right" alt="right" /></li>
+      </ul>
+      <article className="progress">
+        <div className="progress-play"/>
+      </article>
+      <audio src="foo.ogg">
+        <track kind="captions" src="foo.en.vtt" srclang="en" label="English" />
+        <track kind="captions" src="foo.sv.vtt" srclang="sv" label="Svenska" />
+      </audio>
+    </section>
   );
 };
