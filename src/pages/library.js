b70c03f31cb8c24cf5b05a2bea2bd588a9eff600
everton paixao <everton.paixao16@gmail.com>
2019-05-13 14:40:12 -0300

    page Library

---
+++ src/pages/library.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+// COMPONENTS
+import { Navigation } from '../components/header/index';
+import { Button } from '../components/buttons/index';
+import { ModalPlaylist } from '../components/modal/index';
+
+export default class Library extends Component {
+  render() {
+    return (
+     <div className="content">
+       <Navigation 
+       content={['Playlist', 'Feito para você', 'Musicas', 'Albuns', 'Artistas','Podcasts']}
+      />
+      <div className="create">
+        <ModalPlaylist/>
+      </div>
+     </div>
+    );
+  }
+}
