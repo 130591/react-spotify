a2b9411fdf570d9664210c24f93e45bf6d00eaa9
everton paixao <everton.paixao16@gmail.com>
2019-05-13 14:32:31 -0300

    novas rotas search/playlist

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,21 @@
 import React from "react";
 import { Route, Switch } from "react-router";
-import { BrowserRouter } from "react-router-dom";
+import { Router, BrowserRouter, browserHistory } from "react-router-dom";
 
 // COMPONENTS PAGES
 import Home from "./pages/home";
 import SingIn from "./pages/login";
+import Search from './pages/search';
+import Library from './pages/library';
 
 // ROUTES
 export const Routes = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={SingIn} />
-      <Route exact path="/home" component={Home} />
+      <Route path="/home" component={Home} />
+      <Route path="/search" component={Search} />
+      <Route path="/playlist" component={Library} />
     </Switch>
   </BrowserRouter>
 );
