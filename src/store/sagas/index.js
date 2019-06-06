93da4dfc71d8eecdfe29212d9ca27849e35f486a
everton paixao <everton.paixao16@gmail.com>
2019-06-06 11:29:05 -0300

    index saga criado

---
+++ src/store/sagas/index.js
@@ -0,0 +1,15 @@
+import { all, takeEvery } from "redux-saga/effects";
+
+import { asyncLoadHome } from "./home";
+import { Types as PlaylistsTypes } from "../ducks/playlist";
+
+import { getPlaylists, createPlaylist } from "./playlist";
+import { Types as HomeTypes } from "../ducks/home";
+
+export default function* rootSaga() {
+  yield all([
+    takeEvery(HomeTypes.LOADING, asyncLoadHome),
+    takeEvery(PlaylistsTypes.LOADING, getPlaylists),
+    takeEvery(PlaylistsTypes.DATACREATE, createPlaylist)
+  ]);
+}
