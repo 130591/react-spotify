b144fa3643e04e47295be8acb97508c906e0a5a9
everton paixao <everton.paixao16@gmail.com>
2019-06-06 14:55:27 -0300

    correção botão de player não estava funcionando

--- src/components/player/index.js
+++ src/components/player/index.js
@@ -1,70 +1,40 @@
-import React, { Component } from "react";
-
-import { Howl, Howler } from "howler";
+import React, { useState } from "react";
 
 import play from "../../imagens/play-button.svg";
 import pause from "../../imagens/pause.svg";
 import left from "../../imagens/fast-forward-button (1).svg";
 
-export class Player extends Component {
-  constructor() {
-    super();
-    this.state = {
-      player: {
-        audio: {},
-        tracks: {},
-        tracksID: 0,
-        paused: false,
-        saveSeek: "",
-        currentTrack: ""
-      }
-    };
+export const Player = () => {
+  const [track, setTrack] = useState(false);
+  const [tracksID, settracksID] = useState(false);
+  const [audio, setAudio] = useState(false);
+
+  function handleTrack() {
+    track === false ? setTrack(true) : setTrack(false);
   }
 
-  audioExecute() {
-    if (!this.state.player.tracksID) {
-      this.setState({ track: new Howl(this.state.player.audio) });
-      const id = this.state.player.track.play();
-      this.setState({ tracksID: id });
-    }
-  }
-
-  componentDidMount() {
-    this.setState({
-      audio: { urls: ["sounds.webm"] }
-    });
-  }
-
-  handlePaused = () => {
-    this.state.player.paused === true
-      ? this.setState({ paused: false })
-      : this.setState({ paused: true });
-  };
-
-  render() {
-    return (
-      <section className="player">
-        <ul className="controls">
-          <li>
-            <img src={left} className="controls__left" alt="left" />
+  return (
+    <section className="player">
+      <ul className="controls">
+        <li>
+          <img src={left} className="controls__left" alt="left" />
+        </li>
+        {track === true ? (
+          <li onClick={handleTrack}>
+            <img src={pause} className="controls__play" alt="play" />
           </li>
-          {this.state.player.paused === true ? (
-            <li onClick={this.handlePaused}>
-              <img src={pause} className="controls__play" alt="play" />
-            </li>
-          ) : (
-            <li onClick={this.handlePaused}>
-              <img src={play} className="controls__play" alt="play" />
-            </li>
-          )}
-          <li>
-            <img src={left} className="controls__right" alt="right" />
+        ) : (
+          <li onClick={handleTrack}>
+            <img src={play} className="controls__play" alt="play" />
           </li>
-        </ul>
-        <article className="progress">
-          <div className="progress-play" />
-        </article>
-      </section>
-    );
-  }
-}
+        )}
+        <li>
+          <img src={left} className="controls__right" alt="right" />
+        </li>
+      </ul>
+      <article className="progress">
+        <div className="progress-play" />
+      </article>
+    </section>
+  );
+};
