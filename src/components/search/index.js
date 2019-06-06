893bf64ae2973ed730b3a7e4bf28c1c03694e43d
everton paixao <everton.paixao16@gmail.com>
2019-06-06 11:23:12 -0300

    component search

--- src/components/search/index.js
+++ src/components/search/index.js
@@ -1,13 +1,20 @@
-import React, { Component } from 'react';
+import React from "react";
 
-// import { Container } from './styles';
-
-export const OffSearch = () =>{
-    return (
-      <>
+export const OffSearch = ({ hasResult }) => {
+  return (
+    <>
       <input type="text" className="search" placeholder="Comece a digitar" />
-      {/* <h2 className="search-title">Busque no Spotify</h2>
-      <p className="search-description">Encontre suas músicas, playlists, artistas, álbuns e podcasts favoritos.</p> */}
-      </>
-    );
-}
+      {!!hasResult ? (
+        <>
+          <h2 className="search-title">Busque no Spotify</h2>
+          <p className="search-description">
+            Encontre suas músicas, playlists, artistas, álbuns e podcasts
+            favoritos.
+          </p>
+        </>
+      ) : (
+        ""
+      )}
+    </>
+  );
+};
