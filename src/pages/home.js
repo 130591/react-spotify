c092b2b17a9f67c5fd2150e74921c709c90b2e46
everton paixao <everton.paixao16@gmail.com>
2019-06-06 11:23:45 -0300

    page home

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
 
 // COMPONENTS
 import { Card } from "../components/card/index";
@@ -7,40 +9,36 @@ import { Collections, CollectionGrid } from "../components/colections/index";
 
 // ICONS
 import capa from "../imagens/Richie-Kotzen-Mother-Heads-Family-Reunion-1994.jpg";
+// ACTIONS
+import { Creators as homeActions } from "../store/ducks/home";
 
 class Home extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  componentDidMount = () => {
+    // this.props.getLoad()
+    console.log("LOAD", this.props.getLoad);
+  };
+
   render() {
     return (
       <div className="content">
-        <Navigation content={['Em Destaque', 'Podcasts', 'paradas', 'Gêneros', 'lançamentos','Descobrir']} />
+        <Navigation
+          content={[
+            "Em Destaque",
+            "Podcasts",
+            "paradas",
+            "Gêneros",
+            "lançamentos",
+            "Descobrir"
+          ]}
+        />
         <Collections title="Lançamentos">
+          {this.props.home}
           <CollectionGrid>
-            <Card Image={capa} />
-            <Card Image={capa} />
-            <Card Image={capa} />
-            <Card Image={capa} />
-            <Card Image={capa} />
-            <Card Image={capa} />
-            <Card Image={capa} />
-            <Card Image={capa} />
-            <Card Image={capa} />
-            <Card Image={capa} />
-          </CollectionGrid>
-        </Collections>
-        <Collections title="Lançamentos">
-          <CollectionGrid>
-            <Card Image={capa} />
-            <Card Image={capa} />
-            <Card Image={capa} />
-            <Card Image={capa} />
-            <Card Image={capa} />
-            <Card Image={capa} />
-          </CollectionGrid>
-        </Collections>
-        <Collections title="Lançamentos">
-          <CollectionGrid>
-            <Card Image={capa} />
-            <Card Image={capa} />
             <Card Image={capa} />
             <Card Image={capa} />
             <Card Image={capa} />
@@ -51,4 +49,13 @@ class Home extends Component {
   }
 }
 
-export default Home;
+const mapDispatchToProps = dispatch => {
+  bindActionCreators(homeActions, dispatch);
+};
+
+const mapStateToProps = state => ({ home: state.home });
+
+export default connect(state => ({
+  mapStateToProps,
+  mapDispatchToProps
+}))(Home);
