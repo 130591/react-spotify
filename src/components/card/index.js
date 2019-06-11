8cf941bdc8b5111d984e3f3e1ebcd348680b83aa
everton paixao <everton.paixao16@gmail.com>
2019-06-11 17:30:02 -0300

    passing  data for component card

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -2,16 +2,24 @@ import React from "react";
 
 import disc from "../../imagens/tick.svg";
 
-export const Card = ({ Image }, styled) => {
+export const Card = ({ Image, Title, Artist, Key }, styled) => {
   return (
-    <article className={`card ${styled.card}`}>
+    <article className={`card ${styled.card}`} key={Key}>
       <div className={`card-header ${styled.cardHead}`}>
-        <img src={Image} className={`card-header__cover ${styled.cardCover}`} alt="artista" />
-        <img src={disc} className={`card-header__disc ${styled.cardDisc}`} alt="artista" />
+        <img
+          src={Image}
+          className={`card-header__cover ${styled.cardCover}`}
+          alt="artista"
+        />
+        <img
+          src={disc}
+          className={`card-header__disc ${styled.cardDisc}`}
+          alt="artista"
+        />
       </div>
       <h2 className={`card__title ${styled.cardTitle}`}>
-        Into to Black
-        <span>Richie Koten</span>
+        {Title}
+        <span>{Artist}</span>
       </h2>
     </article>
   );
