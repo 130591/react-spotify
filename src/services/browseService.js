59f3187f8add54240b86a0796cf7fad4d3818363
everton paixao <everton.paixao16@gmail.com>
2019-06-25 21:13:04 -0300

    creating browse service

---
+++ src/services/browseService.js
@@ -0,0 +1,30 @@
+import { api } from "./index";
+
+export class AlbumService {
+  static async fetchCategories(accessToken) {
+    return await api.get(`https://api.spotify.com/v1/browse/categories`, {
+      headers: new Headers({
+        Authorization: "Bearer " + accessToken
+      })
+    });
+  }
+
+  static async fetchNewReleases(accessToken) {
+    return await api.get(`https://api.spotify.com/v1/browse/new-releases`, {
+      headers: new Headers({
+        Authorization: "Bearer " + accessToken
+      })
+    });
+  }
+
+  static async fetchFeatured(accessToken) {
+    return await api.get(
+      `https://api.spotify.com/v1/browse/featured-playlists`,
+      {
+        headers: new Headers({
+          Authorization: "Bearer " + accessToken
+        })
+      }
+    );
+  }
+}
