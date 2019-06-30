a3681c48734e778c938bbf327905526f0ed9c05c
everton paixao <everton.paixao16@gmail.com>
2019-06-30 13:57:40 -0300

    creating token reducer

---
+++ src/store/ducks/token.js
@@ -0,0 +1,40 @@
+import { createReducer, createActions } from "reduxsauce";
+import Immutable from "seamless-immutable";
+
+/* Types & Action Creators */
+
+const { Types, Creators } = createActions({
+  setToken: ["token"],
+  getToken: [],
+});
+
+export const TokenTypes = Types;
+export default Creators;
+
+/* Initial State */
+
+export const INITIAL_STATE = Immutable({
+  token: ""
+});
+
+/* Reducers */
+
+const SetToken = (state = INITIAL_STATE, action) => {
+  return {
+    ...state,
+    token: action.token
+  };
+};
+
+const GetToken = (state = INITIAL_STATE, action) => {
+  return {
+    ...state,
+  };
+};
+
+/* Reducers to types */
+
+export const token = createReducer(INITIAL_STATE, {
+  [Types.SET_TOKEN]: SetToken,
+  [Types.GET_TOKEN]: GetToken
+});
