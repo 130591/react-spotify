5d7d5f39b2f223ec8adfc7df3cb84e6b6e2a3084
everton paixao <everton.paixao16@gmail.com>
2019-06-30 13:59:16 -0300

    creating browser reducer

---
+++ src/store/ducks/browse.js
@@ -0,0 +1,79 @@
+import { createReducer, createActions } from "reduxsauce";
+import Immutable from "seamless-immutable";
+
+/* Types & Action Creators */
+
+const { Types, Creators } = createActions({
+  fetchFeaturedError: [],
+  fetchCategoriesError: [],
+  fetchNewReleasesError: [],
+  fetchFeaturedSuccess: ["featured"],
+  fetchCategoriesSuccess: ["categories"],
+  fetchNewReleasesSuccess: ["newReleases"]
+});
+
+export const BrowseTypes = Types;
+export default Creators;
+
+/* Initial State */
+
+export const INITIAL_STATE = Immutable({
+  // data: [],
+});
+
+/* Reducers */
+const FetchCategoriesSuccess = (state = INITIAL_STATE, action) => {
+  return {
+    ...state,
+    view: action.categories.items,
+    fetchCategoriesError: false
+  };
+};
+
+const FetchCategoriesError = (state = INITIAL_STATE, action) => {
+  return {
+    ...state,
+    fetchCategoriesError: true
+  };
+};
+
+const FetchNewReleasesSuccess = (state = INITIAL_STATE, action) => {
+  return {
+    ...state,
+    view: action.newReleases.items,
+    fetchNewReleasesError: false
+  };
+};
+
+const FetchNewReleasesError = (state = INITIAL_STATE, action) => {
+  return {
+    ...state,
+    fetchNewReleasesError: true
+  };
+};
+
+const FetchFeaturedSuccess = (state = INITIAL_STATE, action) => {
+  return {
+    ...state,
+    view: action.featured.items,
+    fetchFeaturedError: false
+  };
+};
+
+const FetchFeaturedError = (state = INITIAL_STATE, action) => {
+  return {
+    ...state,
+    fetchFeaturedError: true
+  };
+};
+
+/* Reducers to types */
+
+export const reducer = createReducer(INITIAL_STATE, {
+  [Types.FETCH_FEATURED_ERROR]: FetchFeaturedError,
+  [Types.FETCH_FEATURED_ERROR]: FetchCategoriesError,
+  [Types.FETCH_NEW_RELEASES_ERROR]: FetchNewReleasesError,
+  [Types.FETCH_FEATURED_SUCCESS]: FetchFeaturedSuccess,
+  [Types.FETCH_FEATURED_SUCCESS]: FetchCategoriesSuccess,
+  [Types.FETCH_NEW_RELEASES_SUCCESS]: FetchNewReleasesSuccess
+});
