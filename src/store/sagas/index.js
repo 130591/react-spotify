77cf83841153c02dc34a34ce286d1d6b1b975980
everton paixao <everton.paixao16@gmail.com>
2019-07-07 14:05:12 -0300

    config root saga with feature at home

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,14 +1,15 @@
 import { all, takeEvery } from "redux-saga/effects";
 
-import { asyncLoadHome } from "./home";
 import { Types as PlaylistsTypes } from "../ducks/playlist";
+import { AlbumsTypes } from "../ducks/albums";
 
 import { getPlaylists, createPlaylist } from "./playlist";
-import { Types as HomeTypes } from "../ducks/home";
+import { Types } from "../ducks/home";
+import { asyncLoadHome } from "./home";
 
 export default function* rootSaga() {
   yield all([
-    takeEvery(HomeTypes.ASYNCDATA, asyncLoadHome),
+    takeEvery(Types.ASYNCDATA, asyncLoadHome),
     takeEvery(PlaylistsTypes.LOADING, getPlaylists),
     takeEvery(PlaylistsTypes.DATACREATE, createPlaylist)
   ]);
