d9c03fc9ccb3a8d8234b33056dc1f782a6f8eaa2
everton paixao <everton.paixao16@gmail.com>
2019-07-29 21:23:49 -0300

    update home paga

--- src/store/sagas/home.js
+++ src/store/sagas/home.js
@@ -1,15 +1,44 @@
 import { put, call } from "redux-saga/effects";
-import { api } from "../../services";
 
-import { Creators as homeActions } from "../ducks/home";
+// DATA SERVICE
+import { AlbumService } from "../../services/albumService";
+import { UserService } from "../../services/userService";
+import { PlayListService } from "../../services/playlistService";
+
+// ACTIONS
 import { Creators as ErrosActions } from "../ducks/error";
+import Creators from "../ducks/albums";
+import CreatorsToken from "../ducks/token";
+import CreatorsRep from "../ducks/reprodutions";
+import Users from "../ducks/user";
 
 export function* asyncLoadHome() {
   try {
-    const resp = yield call(api.get, "http://localhost:3333/home");
+    yield put(Creators.fetchAlbumsPending());
+    // CALL API
+    const token = yield call(UserService.Token);
+    
+    const user = yield call(UserService.Request, token);
 
-    yield put(homeActions.getData(resp.data));
+    const resp = yield call(AlbumService.fetchAlbums, token);
+
+    const reprodution = yield call(
+      PlayListService.reprodutionList,
+      token,
+      "tracks"
+    );
+
+    // ACTIONS TRIGGERS
+    yield put(CreatorsToken.setToken(token));
+
+    yield put(Creators.fetchAlbumsSuccess(resp.data));
+
+    yield put(Users.fetchUserSuccess(user));
+
+    yield put(CreatorsRep.reprodutionSuccess(reprodution.data));
+
   } catch (err) {
+    yield put(Creators.fetchAlbumsError());
     yield put(
       ErrosActions.setError("dander", "Não foi possível obter os dados.")
     );
