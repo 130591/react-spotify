4226a1af8dd3963018283e48ca3d59e283893608
everton paixao <everton.paixao16@gmail.com>
2019-07-31 21:07:29 -0300

    control volume

--- src/components/player/controlVolume.js
+++ src/components/player/controlVolume.js
@@ -1,36 +1,37 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 class VolumeControls extends Component {
-
   constructor(props) {
-    
     super(props);
     this.state = {
       volume: props.volume
     };
   }
 
-	updateVolume = (e) => {
+  updateVolume = e => {
+    this.setState({
+      volume: e.target.value
+    });
 
-	  this.setState({
-	    volume: e.target.value
-	  });
+    this.props.updateVolume(Math.ceil(e.target.value / 10) * 10);
+  };
 
-	  this.props.updateVolume(Math.ceil(e.target.value / 10) * 10);
-	}
-
-
-	render() {
-
-	  return (
-	    <div className='volume-container'>
-	      <i className="fa fa-volume-up" aria-hidden="true"/>
-	      <input className='volume' type="range" min={0} max={100} value={this.state.volume} onChange={this.updateVolume} />
-	    </div>
-	  );
-    
-	}
+  render() {
+    return (
+      <div className="volume-container">
+        <i className="fa fa-volume-up" aria-hidden="true" />
+        <input
+          className="volume"
+          type="range"
+          min={0}
+          max={100}
+          value={this.state.volume}
+          onChange={this.updateVolume}
+        />
+      </div>
+    );
+  }
 }
 
 VolumeControls.propTypes = {
