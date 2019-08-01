167a48bf8a8b2577fdd157901bc67a2f00529ce0
everton paixao <everton.paixao16@gmail.com>
2019-07-31 21:07:42 -0300

    playlist

--- src/components/modal/PlayList.js
+++ src/components/modal/PlayList.js
@@ -1,7 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 import PropTypes from "prop-types";
+import debounce from "lodash.debounce";
 
 export const PlayList = ({ OnChange }) => {
+  const handleChange = value => {
+    OnChange(value);
+  };
+
+  const emitChange = debounce(handleChange, 2000);
+
   return (
     <div className="contentPlay">
       <span>Nome da Playlist</span>
@@ -9,7 +16,7 @@ export const PlayList = ({ OnChange }) => {
         type="text"
         className="input-play"
         placeholder="Comece a escrever..."
-        onChange={e => OnChange(e)}
+        onChange={e => emitChange(e.target.value)}
       />
     </div>
   );
