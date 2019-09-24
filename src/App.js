0392ddab59fd840f230f1bb7f921af080470ebbe
everton paixao <everton.paixao16@gmail.com>
2019-09-24 18:33:38 -0300

    create feature audio control

--- src/App.js
+++ src/App.js
@@ -1,44 +1,88 @@
 import React, { Component } from "react";
-
-import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
 
-import SideBar from "./components/menu/index";
-import Player from "./components/player/index";
-import { Routes } from "./routes";
-import { Creators } from "./store/ducks/home";
+import { bindActionCreators } from 'redux';
+import PropTypes from 'prop-types';
+
+//COMPONENTS
+import { Routes } from './routes';
+import Player from './components/player';
+import Creators from './store/ducks/song';
 
 class App extends Component {
-  constructor(props) {
-    super();
-  }
+	static audio;
 
-  async componentDidMount() {
-    this.props.request();
-  }
+	componentWillReceiveProps(nextProps) {
+		if (this.audio !== undefined) {
+			this.audio.volume = nextProps.volume / 100;
+		}
+	}
 
-  render() {
-    return (
-      <>
-        <div id="wrapper">
-          <SideBar />
-          <Routes />
-        </div>
-        <Player />
-      </>
-    );
-  }
+	stopSong = () => {
+		if (this.audio) {
+			this.props.stopSong();
+			this.audio.pause();
+		}
+	}
+
+	pauseSong = () => {
+		if (this.audio) {
+			this.props.pauseSong();
+			this.audio.pause();
+		}
+	}
+
+	resumeSong = () => {
+		if (this.audio) {
+			this.props.resumeSong();
+			this.audio.play();
+		}
+	}
+
+	audioControl = (song) => {
+		const { playSong, stopSong } = this.props;
+
+		if (this.audio === undefined) {
+			playSong(song);
+			this.audio = new Audio(song.preview_url);
+			this.audio.play();
+		} else {
+			stopSong();
+			this.audio.pause();
+			playSong(song.track);
+			this.audio = new Audio(song.preview_url);
+			this.audio.play();
+		}
+	}
+
+	render() {
+		return (
+			<>
+				<div id="wrapper">
+					<Routes
+						audioControl={this.audioControl}
+					/>
+				</div>
+				<Player
+					stop={this.stopSong}
+					pause={this.pauseSong}
+					resume={this.resumeSong}
+					audioControl={this.audioControl}
+				/>
+			</>
+		);
+	}
 }
 
-const mapStateToProps = state => {
-  return {
-    token: state.token
-  };
+const mapStateToProps = (state) => {
+	return {
+		token: state.token,
+		volume: state.sound.volume
+	};
 };
 
-const mapDispatchToProps = dispatch => bindActionCreators(Creators, dispatch);
+const mapDispatchToProps = dispatch => {
+	return bindActionCreators({ ...Creators }, dispatch);
+};
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
