38e1fe2b8f04565b6e9c73d28162ef1d0809b28a
everton paixao <everton.paixao16@gmail.com>
2019-09-24 18:06:48 -0300

    update saga construct

--- src/store/sagas/user.js
+++ src/store/sagas/user.js
@@ -2,17 +2,8 @@ import { call } from "redux-saga/effects";
 import { UserService } from "../../services/userService";
 import Creators from "../ducks/token";
 
-import { BrowserService } from "../../services/browseService";
-
 export function* userToken() {
   const token = yield call(UserService.Token);
 
   Creators.setToken(token);
-}
-
-export function* recently() {
-  const token = yield call(UserService.Token);
-
-  // const resp = yield call(BrowserService.fetchRecentlyPlay, token, 5);
-  // console.log(resp);
 }
