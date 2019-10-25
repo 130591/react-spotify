8ca67d1b56220ee3b0d559892a95cf6a7cf3f2b4
Everton Paixão <everton.paixao16@gmail.com>
2019-10-24 21:13:23 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,4 +2,4 @@
 // VARIABLE CONFIG
 export const client_id = "f5bec2e34a7a4231b0d2e84afd055ec2";
 export const client_secret = "CLIENT_SECRET";
-export const redirect_uri = "https://accounts.spotify.com/authorize?client_id=230be2f46909426b8b80cac36446b52a&scope=playlist-read-private%20playlist-read-collaborative%20playlist-modify-public%20user-read-recently-played%20playlist-modify-private%20ugc-image-upload%20user-follow-modify%20user-follow-read%20user-library-read%20user-library-modify%20user-read-private%20user-read-email%20user-top-read%20user-read-playback-state&response_type=token&redirect_uri=https://spotify-1.herokuapp.com/";
+export const redirect_uri = "https://accounts.spotify.com/authorize?client_id=230be2f46909426b8b80cac36446b52a&scope=playlist-read-private%20playlist-read-collaborative%20playlist-modify-public%20user-read-recently-played%20playlist-modify-private%20ugc-image-upload%20user-follow-modify%20user-follow-read%20user-library-read%20user-library-modify%20user-read-private%20user-read-email%20user-top-read%20user-read-playback-state&response_type=token&redirect_uri=https://spotify-1.herokuapp.com/callback";
